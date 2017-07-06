9bfa736097b4f34191f5cc28638d4938b190314f
rosswu24 <wuyawchia24@gmail.com>
2017-07-05 19:19:27 -1000

    can't get to broadcast

--- server.js
+++ server.js
@@ -4,44 +4,28 @@ const server = net.createServer((socket) =>{
 
   // read input data from client side
  socket.on('data',function(data){ 
-  // var clientMsg = data;
-  // var msg = process.stdout.write(clientMsg.toString());
-  // clients.forEach((client)=>{
-  //  clients.push(socket);
-  // })
 
-    function broadcast(message, sender){
-      clients.forEach((client)=>{
-        if(client === sender){
-          return;
-        }else{
-          client.write(message); 
-        // clients.push(socket);
-        }
-    });
+	function broadcast(sender){
+	  clients.forEach((client)=>{
+		if(client === sender){
+		  return;
+		}else{
+		process.stdout.write(data.toString());
+		}
+	});
   }
 
   // debuffers data from client
-  process.stdout.write(data.toString());
-  });
-
+  // process.stdout.write(data.toString());
+  // });
 
   // client disconnect close out socket
   socket.on('close',()=>{ 
-    process.stdout.write('\n' + 'disconnect');
+	process.stdout.write('\n' + 'disconnect');
   });
 
 }).listen (6969, '0.0.0.0');
 
 
-const broadcastMsg = net.createConnection({port:6969},()=>{
-
-  process.stdin.on('readable', () => {
-    var chunk = process.stdin.read();
-      if (chunk !== null) {
-       process.stdout.write(`data: ${chunk}`);
-      }
-  });
-});
 
 
